ae9a87d65feb062a10f6f6131f49a89a5bc6452d
Azizi <gangmemberzizi@outlook.com>
2025-07-11 16:24:39 -0500

    Add files via upload

--- tls_config.go
+++ tls_config.go
@@ -5,78 +5,80 @@ import (
 	tls "github.com/refraction-networking/utls"
 )
 
-var TLSConfig = tls.ClientHelloSpec{
-	CipherSuites: []uint16{
-		tls.GREASE_PLACEHOLDER,
-		tls.TLS_AES_128_GCM_SHA256,
-		tls.TLS_AES_256_GCM_SHA384,
-		tls.TLS_CHACHA20_POLY1305_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-	},
-	CompressionMethods: []byte{
-		tls2.CompressionNone,
-	},
-	Extensions: []tls.TLSExtension{
-		&tls.UtlsGREASEExtension{},
-		&tls.SessionTicketExtension{},
-		&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
-			tls.ECDSAWithP256AndSHA256,
-			tls.PSSWithSHA256,
-			tls.PKCS1WithSHA256,
-			tls.ECDSAWithP384AndSHA384,
-			tls.PSSWithSHA384,
-			tls.PKCS1WithSHA384,
-			tls.PSSWithSHA512,
-			tls.PKCS1WithSHA512,
-		}},
-		&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
-			{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
-			{Group: tls.X25519MLKEM768},
-			{Group: tls.X25519},
-		}},
-		&tls.SCTExtension{},
-		&tls.SupportedPointsExtension{SupportedPoints: []byte{
-			tls2.PointFormatUncompressed,
-		}},
-		&tls.SupportedVersionsExtension{Versions: []uint16{
+func TLSConfig() *tls.ClientHelloSpec {
+	return &tls.ClientHelloSpec{
+		CipherSuites: []uint16{
 			tls.GREASE_PLACEHOLDER,
-			tls.VersionTLS13,
-			tls.VersionTLS12,
-		}},
-		&tls.StatusRequestExtension{},
-		&tls.ALPNExtension{AlpnProtocols: []string{
-			"http/1.1",
-		}},
-		&tls.SNIExtension{},
-		tls.BoringGREASEECH(),
-		&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
-			tls.CertCompressionBrotli,
-		}},
-		&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
-			tls.GREASE_PLACEHOLDER,
-			tls.X25519MLKEM768,
-			tls.X25519,
-			tls.CurveP256,
-			tls.CurveP384,
-		}},
-		&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
-			tls.PskModeDHE,
-		}},
-		&tls.ExtendedMasterSecretExtension{},
-		&tls.RenegotiationInfoExtension{
-			Renegotiation: tls.RenegotiateOnceAsClient,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
-		&tls.UtlsGREASEExtension{},
-	},
+		CompressionMethods: []byte{
+			tls2.CompressionNone,
+		},
+		Extensions: []tls.TLSExtension{
+			&tls.UtlsGREASEExtension{},
+			&tls.SessionTicketExtension{},
+			&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+				tls.ECDSAWithP256AndSHA256,
+				tls.PSSWithSHA256,
+				tls.PKCS1WithSHA256,
+				tls.ECDSAWithP384AndSHA384,
+				tls.PSSWithSHA384,
+				tls.PKCS1WithSHA384,
+				tls.PSSWithSHA512,
+				tls.PKCS1WithSHA512,
+			}},
+			&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+				{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+				{Group: tls.X25519MLKEM768},
+				{Group: tls.X25519},
+			}},
+			&tls.SCTExtension{},
+			&tls.SupportedPointsExtension{SupportedPoints: []byte{
+				tls2.PointFormatUncompressed,
+			}},
+			&tls.SupportedVersionsExtension{Versions: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.VersionTLS13,
+				tls.VersionTLS12,
+			}},
+			&tls.StatusRequestExtension{},
+			&tls.ALPNExtension{AlpnProtocols: []string{
+				"http/1.1",
+			}},
+			&tls.SNIExtension{},
+			tls.BoringGREASEECH(),
+			&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+				tls.CertCompressionBrotli,
+			}},
+			&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+				tls.GREASE_PLACEHOLDER,
+				tls.X25519MLKEM768,
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+			}},
+			&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+				tls.PskModeDHE,
+			}},
+			&tls.ExtendedMasterSecretExtension{},
+			&tls.RenegotiationInfoExtension{
+				Renegotiation: tls.RenegotiateOnceAsClient,
+			},
+			&tls.UtlsGREASEExtension{},
+		},
+	}
 }
