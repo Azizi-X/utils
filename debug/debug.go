a4df2a18c98e7a0f6f8e6cfdd669b5bf2336e5d7
Azizi <gangmemberzizi@outlook.com>
2025-07-12 11:52:38 -0500

    Add files via upload

--- debug/debug.go
+++ debug/debug.go
@@ -1,17 +1,8 @@
 package debug
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"os"
-	"path/filepath"
-	"runtime"
-	"strings"
 	"sync"
-	"time"
-
-	"github.com/shirou/gopsutil/mem"
 )
 
 const (
@@ -24,134 +15,6 @@ type Debugger struct {
 	maxDepth int
 	strip    bool
 	Calls    int
-}
-
-type MemStats struct {
-	Total       uint64
-	Alloc       uint64
-	TotalAlloc  uint64
-	Mallocs     uint64
-	HeapAlloc   uint64
-	HeapObjects uint64
-}
-
-type Stack struct {
-	Error    string
-	Time     int64
-	Frames   []StackFrame
-	MemStats MemStats
-	Total    int
-}
-
-type StackFrame struct {
-	Function string
-	File     string
-	Line     int
-}
-
-type stackOptions struct {
-	strip    bool
-	maxDepth int
-	calls    int
-}
-
-func (s *Stack) Hash(extra ...string) string {
-	var builder strings.Builder
-
-	builder.WriteString(s.Error)
-
-	for i := range extra {
-		builder.WriteString(extra[i])
-	}
-
-	for _, frame := range s.Frames {
-		builder.WriteString(frame.Function)
-		builder.WriteString(frame.File)
-		builder.WriteString(string(rune(frame.Line)))
-	}
-
-	sum := sha256.Sum256([]byte(builder.String()))
-	return hex.EncodeToString(sum[:])
-}
-
-func stripPath(frameFile string) string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return frameFile
-	}
-
-	rel, err := filepath.Rel(cwd, frameFile)
-	if err != nil {
-		return frameFile
-	}
-
-	return filepath.ToSlash(rel)
-}
-
-func frames(skip int, options stackOptions) (stack []StackFrame) {
-	pc := make([]uintptr, options.maxDepth)
-	n := runtime.Callers(skip, pc)
-	frames := runtime.CallersFrames(pc[:n])
-
-	for {
-		frame, more := frames.Next()
-		if !strings.HasPrefix(frame.Function, "runtime.") {
-			var file string
-
-			if options.strip {
-				file = stripPath(frame.File)
-			} else {
-				file = frame.File
-			}
-
-			stack = append(stack, StackFrame{
-				Function: frame.Function,
-				File:     file,
-				Line:     frame.Line,
-			})
-		}
-		if !more {
-			return stack
-		}
-	}
-}
-
-func memStats() MemStats {
-	var total uint64
-	var stats runtime.MemStats
-
-	runtime.ReadMemStats(&stats)
-
-	if memory, _ := mem.VirtualMemory(); memory != nil {
-		total = memory.Total
-	}
-
-	return MemStats{
-		Total:       total,
-		Alloc:       stats.Alloc,
-		TotalAlloc:  stats.TotalAlloc,
-		Mallocs:     stats.Mallocs,
-		HeapAlloc:   stats.HeapAlloc,
-		HeapObjects: stats.HeapObjects,
-	}
-}
-
-func makeStack(err error, skip int, options stackOptions) Stack {
-	frames := frames(skip, options)
-	memStats := memStats()
-
-	errStr := ""
-	if err != nil {
-		errStr = err.Error()
-	}
-
-	return Stack{
-		Error:    errStr,
-		Time:     time.Now().UnixMilli(),
-		Frames:   frames,
-		MemStats: memStats,
-		Total:    options.calls,
-	}
 }
 
 func (d *Debugger) Publish(msg any, formats ...any) error {
---
+++ debug/stack.go
@@ -0,0 +1,55 @@
+package debug
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+)
+
+type MemStats struct {
+	Total       uint64
+	Alloc       uint64
+	TotalAlloc  uint64
+	Mallocs     uint64
+	HeapAlloc   uint64
+	HeapObjects uint64
+}
+
+type Stack struct {
+	Error    string
+	Time     int64
+	Frames   []StackFrame
+	MemStats MemStats
+	Total    int
+}
+
+type StackFrame struct {
+	Function string
+	File     string
+	Line     int
+}
+
+type stackOptions struct {
+	strip    bool
+	maxDepth int
+	calls    int
+}
+
+func (s *Stack) Hash(extra ...string) string {
+	var builder strings.Builder
+
+	builder.WriteString(s.Error)
+
+	for i := range extra {
+		builder.WriteString(extra[i])
+	}
+
+	for _, frame := range s.Frames {
+		builder.WriteString(frame.Function)
+		builder.WriteString(frame.File)
+		builder.WriteString(string(rune(frame.Line)))
+	}
+
+	sum := sha256.Sum256([]byte(builder.String()))
+	return hex.EncodeToString(sum[:])
+}
