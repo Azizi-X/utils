d1551e761e4ae6b96ce22b81253d79e55f24d4fd
Azizi <190982731+Azizi-X@users.noreply.github.com>
2025-10-16 02:37:04 -0400

    Create atomic_string.go

---
+++ atomic/atomic_string.go
@@ -0,0 +1,51 @@
+package atomic
+
+import "sync/atomic"
+
+type nocmp [0]func()
+
+type AtomicString struct {
+	_ nocmp
+	atomic.Value
+}
+
+func NewString(val string) *AtomicString {
+	str := &AtomicString{}
+	if val != "" {
+		str.Store(val)
+	}
+	return str
+}
+
+func (str *AtomicString) Load() string {
+	return unpackString(str.Value.Load())
+}
+
+func (str *AtomicString) Store(val string) {
+	str.Value.Store(packString(val))
+}
+
+func (str *AtomicString) CompareAndSwap(old, new string) (swapped bool) {
+	if str.Value.CompareAndSwap(packString(old), packString(new)) {
+		return true
+	}
+
+	if old == "" {
+		return str.Value.CompareAndSwap(nil, packString(new))
+	}
+
+	return false
+}
+
+func (str *AtomicString) Swap(val string) (old string) {
+	return unpackString(str.Value.Swap(packString(val)))
+}
+
+func packString(s string) any {
+	return s
+}
+
+func unpackString(v any) string {
+	s, _ := v.(string)
+	return s
+}
