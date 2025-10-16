63172dda3e7644bbb164084323422d448a869133
Azizi <190982731+Azizi-X@users.noreply.github.com>
2025-10-16 07:15:59 -0400

    rename

--- atomic/value.go
+++ atomic/value.go
@@ -2,23 +2,23 @@ package atomic
 
 import "sync/atomic"
 
-type AtomicV[T any] struct {
+type V[T any] struct {
 	_ nocmp
 	atomic.Value
 }
 
-func (z *AtomicV[T]) Load() T {
+func (z *V[T]) Load() T {
 	return unpackV[T](z.Value.Load())
 }
 
-func (z *AtomicV[T]) Store(val T) {
+func (z *V[T]) Store(val T) {
 	z.Value.Store(packV(val))
 }
 
-func (z *AtomicV[T]) CompareAndSwap(old, new T) (swapped bool) {
+func (z *V[T]) CompareAndSwap(old, new T) (swapped bool) {
 	return z.Value.CompareAndSwap(packV(old), packV(new))
 }
 
-func (z *AtomicV[T]) Swap(val T) (old T) {
+func (z *V[T]) Swap(val T) (old T) {
 	return unpackV[T](z.Value.Swap(packV(val)))
 }
