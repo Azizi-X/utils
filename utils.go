512f6c11ded594fb8f8d98c8f71431d29d13f376
Azizi <190982731+Azizi-X@users.noreply.github.com>
2025-12-14 15:21:08 -0500

    init

--- utils.go
+++ utils.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package utils
 
 import "sync"
@@ -18,24 +17,3 @@ func HandleGroups(fns []func()) {
 
 	wg.Wait()
 }
-=======
-package utils
-
-import "sync"
-
-func BasicEqual[T comparable](a, b T) bool { return a == b }
-
-func HandleGroups(fns []func()) {
-	wg := new(sync.WaitGroup)
-
-	for _, fn := range fns {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fn()
-		}()
-	}
-
-	wg.Wait()
-}
->>>>>>> 2330299ae49ffbbd1dbfff4a195c0169abca5303
